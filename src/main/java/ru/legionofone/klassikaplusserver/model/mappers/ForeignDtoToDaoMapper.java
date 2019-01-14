091eebf7710e7e813c5fed24f63d7f719b8cbecb
the_maker <fshmorgilov@gmail.com>
2019-01-14 19:36:49 +0300

    Foreign to DTO mapper

---
+++ src/main/java/ru/legionofone/klassikaplusserver/model/mappers/ForeignDtoToDaoMapper.java
@@ -0,0 +1,27 @@
+package ru.legionofone.klassikaplusserver.model.mappers;
+
+import ru.legionofone.klassikaplusserver.model.mappers.base.Mapping;
+import ru.legionofone.klassikaplusserver.model.persistance.entities.DbItem;
+import ru.legionofone.klassikaplusserver.web.dto.obtained.ItemDto;
+
+public class ForeignDtoToDaoMapper extends Mapping<ItemDto, DbItem> {
+
+    @Override
+    public DbItem map(ItemDto itemDto) {
+        DbItem item = new DbItem();
+        item.setExtId(itemDto.getId());
+        if (itemDto.getDescription() != null) item.setDescription(itemDto.getDescription());
+        item.setPrice(Double.valueOf(itemDto.getPrice()));
+        if (itemDto.getImage() != null && !"".equals(itemDto.getImage()))
+            item.setPhoto(itemDto.getImage());
+        else if (itemDto.getPhotogallery().getImage() != null && !"".equals(itemDto.getPhotogallery().getImage()))
+            item.setPhoto(itemDto.getPhotogallery().getImage());
+        item.setName(itemDto.getPagetitle());
+        item.setIcon(itemDto.getImgMini());
+        item.setNovelty(Boolean.valueOf(itemDto.getNew()));
+        item.setPageAlias(itemDto.getPagetitle());
+//todo конвертер для галлереи изображений
+        //todo validator
+        return null;
+    }
+}
