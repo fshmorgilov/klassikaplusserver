9968adb9baadf23b77faa27cc35b95510dd83dbd
the_maker <fshmorgilov@gmail.com>
2019-02-12 13:06:43 +0300

    working login

--- src/main/java/ru/legionofone/klassikaplusserver/web/WebSecurityConfig.java
+++ src/main/java/ru/legionofone/klassikaplusserver/web/WebSecurityConfig.java
@@ -8,6 +8,8 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
@@ -15,20 +17,41 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
-            .authorizeRequests()
-                .antMatchers("/catalog").permitAll();
+    http
+        .authorizeRequests()
+                .antMatchers("/catalog").permitAll()
+                .antMatchers("/login*").permitAll()
+            .anyRequest().authenticated()
+        .and()
+            .formLogin()
+                .loginPage("/login")
+                .permitAll()
+                .defaultSuccessUrl("/homepage.html", true)
+//        .failureUrl("login.html?error=true"); todo Сделать нормальную страничку.
+        .and()
+            .logout()
+                .logoutUrl("/perform_logout")
+                .permitAll()
+                .logoutSuccessUrl("/hello")
+                .deleteCookies("JSESSIONID") // FIXME: 2/12/2019 Ну прям хз
+        .and()
+            .httpBasic();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
     @Bean
     @Override
     public UserDetailsService userDetailsService() {
         UserDetails user =
-             User.withDefaultPasswordEncoder()
-                .username("user")
-                .password("password")
-                .roles("USER")
-                .build();
+                User.withDefaultPasswordEncoder()
+                        .username("u")
+                        .password("p")
+                        .roles("USER")
+                        .build();
 
         return new InMemoryUserDetailsManager(user);
     }
