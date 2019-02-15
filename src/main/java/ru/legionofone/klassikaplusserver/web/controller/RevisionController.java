9a129cd8526eb9e8bdfbdef39ba76a8bea964787
the_maker <fshmorgilov@gmail.com>
2019-02-15 22:55:53 +0300

    Revision controller finished

--- src/main/java/ru/legionofone/klassikaplusserver/web/controller/RevisionController.java
+++ src/main/java/ru/legionofone/klassikaplusserver/web/controller/RevisionController.java
@@ -1,5 +1,24 @@
 package ru.legionofone.klassikaplusserver.web.controller;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import ru.legionofone.klassikaplusserver.service.CatalogService;
+
+@RestController("revision")
 public class RevisionController {
+
+    final CatalogService service;
+
+    @Autowired
+    public RevisionController(CatalogService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public Integer getRevision(@RequestParam(required = false) String deviceId) {
+        return service.getRevision();
+    }
 }
