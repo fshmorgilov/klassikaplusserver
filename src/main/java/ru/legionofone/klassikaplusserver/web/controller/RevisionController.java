17bd558486530f148855217ac3dffd494fb187b6
the_maker <fshmorgilov@gmail.com>
2019-02-18 17:08:06 +0300

    working mvc

--- src/main/java/ru/legionofone/klassikaplusserver/web/controller/RevisionController.java
+++ src/main/java/ru/legionofone/klassikaplusserver/web/controller/RevisionController.java
@@ -2,12 +2,15 @@ package ru.legionofone.klassikaplusserver.web.controller;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import ru.legionofone.klassikaplusserver.service.CatalogService;
 
-@RestController("revision")
+import javax.servlet.http.HttpServletRequest;
+
+@RestController
 public class RevisionController {
 
     final CatalogService service;
@@ -17,8 +20,19 @@ public class RevisionController {
         this.service = service;
     }
 
-    @GetMapping
+    @GetMapping("revision")
     public Integer getRevision(@RequestParam(required = false) String deviceId) {
         return service.getRevision();
+    }
+
+    @GetMapping("Greeting")
+    public String greeting(@RequestParam(name = "name", required = false, defaultValue = "Anonymous") String name, Model model){
+        model.addAttribute("name", model);
+        return "greeting";
+    }
+
+    @GetMapping("admin")
+    public String admin(HttpServletRequest request){
+        return "admin";
     }
 }
